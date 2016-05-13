d50d88ec5777c8a592fb17822bae942f992f04d8
Luan Pablo <luanpab@gmail.com>
2016-05-13 10:21:05 -0300

    added appengine configs

--- radiolabs/settings.py
+++ radiolabs/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '..', 'static'))
 
 # YouTube settings
 YT_KEY = os.environ.get('YT_KEY', '')
