01c8e4c35d96436dae66ce932174370285dd4a4f
Luan Pablo <luanpab@gmail.com>
2016-05-16 16:02:39 -0300

    added MySQLdb lib, fixed project path
    added static files to project

--- manage.py
+++ manage.py
@@ -2,6 +2,12 @@
 import os
 import sys
 
+from radiolabs.boot import fix_path
+
+
+fix_path()
+
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "radiolabs.settings")
 
