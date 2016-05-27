813d8e1c72c0b08e3b0138b998f3173731aafb2f
Luan Pablo <luanpab@gmail.com>
2016-05-27 15:17:34 -0300

    added font-awesome and more feedback when adding videos

--- base/static/js/base.js
+++ base/static/js/base.js
@@ -1,5 +1,12 @@
 $(function () {
     $('.btn-add').click(function () {
+        $('.error-add-msg').text('');
+        var that = $(this);
+
+        var $strong = that.find('strong');
+        var $div = $('<div>').addClass('loading');
+        $div.append($strong);
+        that.html($div);
         var jqxhr = $.ajax({
             url: VIDEO_ADD_URL,
             method: 'POST',
@@ -10,6 +17,13 @@ $(function () {
         });
         jqxhr.done(function () {
             $('input[name="url"]').val('');
+        });
+        jqxhr.fail(function () {
+            $('.error-add-msg').text('Oops... :(');
+        });
+        jqxhr.always(function () {
+            $('input[name="url"]').val('');
+            that.html($strong);
         });
     });
 });
